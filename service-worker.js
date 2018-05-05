0dff25744e86a8796338427e1f297e34ca71add0
Oludotun Longe <funnica4@gmail.com>
2018-05-05 10:41:21 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,35 +1,20 @@
 "use strict";
 var precacheConfig = [
-    ["/Sela/index.html", "464e6a9eadaa2e502dfb5a92500fb3da"],
-    ["/Sela/static/js/main.99ddfe66.js", "e608999c07c3d87b5501794d0543d8c1"],
+    ["/index.html", "2d20eade750cfbc9b3d290de66cdb78c"],
+    ["/static/js/main.019522c1.js", "7c3e6ac1db0ebc9e9b727c2800060976"],
     [
-      "/Sela/static/media/background.496f16a1.svg",
+      "/static/media/background.496f16a1.svg",
       "496f16a1c1dda194cdfbec00200789ef"
     ],
+    ["/static/media/chevron.58a28c1f.svg", "58a28c1f63da5c90bf94f7b812fa968f"],
+    ["/static/media/location.5b4c42d9.svg", "5b4c42d9fbac8f42f1a96dd018665840"],
+    ["/static/media/marker.87d7d574.svg", "87d7d574d04fc0091c97a3515e481691"],
+    ["/static/media/money.936d6e7f.svg", "936d6e7f76799cdd14bbed324dedba0f"],
     [
-      "/Sela/static/media/chevron.58a28c1f.svg",
-      "58a28c1f63da5c90bf94f7b812fa968f"
-    ],
-    [
-      "/Sela/static/media/location.5b4c42d9.svg",
-      "5b4c42d9fbac8f42f1a96dd018665840"
-    ],
-    [
-      "/Sela/static/media/marker.87d7d574.svg",
-      "87d7d574d04fc0091c97a3515e481691"
-    ],
-    [
-      "/Sela/static/media/money.936d6e7f.svg",
-      "936d6e7f76799cdd14bbed324dedba0f"
-    ],
-    [
-      "/Sela/static/media/right-chevron.5c4e7d10.svg",
+      "/static/media/right-chevron.5c4e7d10.svg",
       "5c4e7d108a87243e830734cdf4f57a0a"
     ],
-    [
-      "/Sela/static/media/target.902b549b.svg",
-      "902b549b863ecaf59c76edea84174fc0"
-    ]
+    ["/static/media/target.902b549b.svg", "902b549b863ecaf59c76edea84174fc0"]
   ],
   cacheName =
     "sw-precache-v3-sw-precache-webpack-plugin-" +
@@ -50,16 +35,16 @@ var precacheConfig = [
         })
       : Promise.resolve(t);
   },
-  createCacheKey = function(e, t, n, a) {
-    var r = new URL(e);
+  createCacheKey = function(e, t, n, r) {
+    var a = new URL(e);
     return (
-      (a && r.pathname.match(a)) ||
-        (r.search +=
-          (r.search ? "&" : "") +
+      (r && a.pathname.match(r)) ||
+        (a.search +=
+          (a.search ? "&" : "") +
           encodeURIComponent(t) +
           "=" +
           encodeURIComponent(n)),
-      r.toString()
+      a.toString()
     );
   },
   isPathWhitelisted = function(e, t) {
@@ -96,9 +81,9 @@ var precacheConfig = [
     precacheConfig.map(function(e) {
       var t = e[0],
         n = e[1],
-        a = new URL(t, self.location),
-        r = createCacheKey(a, hashParamName, n, /\.\w{8}\./);
-      return [a.toString(), r];
+        r = new URL(t, self.location),
+        a = createCacheKey(r, hashParamName, n, /\.\w{8}\./);
+      return [r.toString(), a];
     })
   );
 function setOfCachedUrls(e) {
@@ -117,8 +102,8 @@ self.addEventListener("install", function(e) {
   e.waitUntil(
     caches
       .open(cacheName)
-      .then(function(a) {
-        return setOfCachedUrls(a).then(function(n) {
+      .then(function(r) {
+        return setOfCachedUrls(r).then(function(n) {
           return Promise.all(
             Array.from(urlsToCacheKeys.values()).map(function(t) {
               if (!n.has(t)) {
@@ -132,7 +117,7 @@ self.addEventListener("install", function(e) {
                         e.status
                     );
                   return cleanResponse(e).then(function(e) {
-                    return a.put(t, e);
+                    return r.put(t, e);
                   });
                 });
               }
@@ -171,14 +156,14 @@ self.addEventListener("install", function(e) {
           t.request.url,
           ignoreUrlParametersMatching
         ),
-        a = "index.html";
+        r = "index.html";
       (e = urlsToCacheKeys.has(n)) ||
-        ((n = addDirectoryIndex(n, a)), (e = urlsToCacheKeys.has(n)));
-      var r = "/Sela/index.html";
+        ((n = addDirectoryIndex(n, r)), (e = urlsToCacheKeys.has(n)));
+      var a = "/index.html";
       !e &&
         "navigate" === t.request.mode &&
         isPathWhitelisted(["^(?!\\/__).*"], t.request.url) &&
-        ((n = new URL(r, self.location).toString()),
+        ((n = new URL(a, self.location).toString()),
         (e = urlsToCacheKeys.has(n))),
         e &&
           t.respondWith(
