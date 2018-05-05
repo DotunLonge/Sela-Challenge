91c5f728917870b2ac0a5d3a6620be70a831bff4
Oludotun Longe <funnica4@gmail.com>
2018-05-05 09:51:27 +0100

    cleanup

--- src/config.js
+++ src/config.js
@@ -1,18 +1,17 @@
+let useUrl;
 
-let  useUrl;
-
-if(process.env.NODE_ENV === "development"){
-  useUrl = "http://localhost:7000"
-}else{
-  useUrl = "http://sela.fr.openode.io"
+if (process.env.NODE_ENV === "development") {
+  useUrl = "http://localhost:7000";
+} else {
+  useUrl = "http://sela.fr.openode.io";
 }
 
 export default {
   useUrl,
   map: {
     googleMapsAPIKey: process.env.REACT_APP_MAP_API_KEY,
-    defaultLongitude: 131.044,
-    defaultLatitude:   -25.363,
+    defaultLongitude: 6.465422,
+    defaultLatitude: 3.406448,
     defaultZoomLevel: 6
-}
-}
+  }
+};
