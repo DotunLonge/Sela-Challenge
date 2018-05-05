c48b9d78c4a7d5dcde7d7fa73dc70049ee6dd423
Oludotun Longe <funnica4@gmail.com>
2018-05-05 10:05:50 +0100

    cleanup

--- src/components/Map/markers.js
+++ src/components/Map/markers.js
@@ -1,23 +1,26 @@
 import React from "react";
-import colors from "../../helpers/colors";
+import colors from "../../helpers/colors.json";
 
 import { Marker } from "./map-view.style";
 let counter = 0;
 
-export default (projects) => {
-return projects.map((project,index)=>{
+export default projects => {
+  return projects.map((project, index) => {
     counter = counter + 1;
-    if( counter >= colors.length ) counter = 0;
+    if (counter >= colors.length) counter = 0;
     const width = project.keyword.length * 13;
 
-    return <Marker
-    key={index}
-    lat = {project.location.lat}
-    lng = {project.location.lng}
-    markerWidth={width}
-    backgroundColor = {colors[project.projectStatus]}>
-    <span id="arrow" />
-      {project.keyword}
-    </Marker>
-  })
-}
+    return (
+      <Marker
+        key={index}
+        lat={project.location.lat}
+        lng={project.location.lng}
+        markerWidth={width}
+        backgroundColor={colors[project.projectStatus]}
+      >
+        <span id="arrow" />
+        {project.keyword}
+      </Marker>
+    );
+  });
+};
--- src/components/Projects/Card/card.style.js
+++ src/components/Projects/Card/card.style.js
@@ -1,83 +1,82 @@
-import styled from 'styled-components';
-import rightChevron from "../../../assets/icons/right-chevron.svg"
-import colors from "../../../helpers/colors";
+import styled from "styled-components";
+import rightChevron from "../../../assets/icons/right-chevron.svg";
+import colors from "../../../helpers/colors.json";
 
-export const Wrapper = styled.div `
-.text-side{
+export const Wrapper = styled.div`
+  .text-side {
     padding: 0 2.5%;
     position: relative;
 
-    h3{
-    font-style: normal;
-    font-weight: 300;
-    line-height: normal;
-    font-size: 23px;
-    margin: 30px 0 5px;
-    color: #333333;
+    h3 {
+      font-style: normal;
+      font-weight: 300;
+      line-height: normal;
+      font-size: 23px;
+      margin: 30px 0 5px;
+      color: #333333;
     }
 
-    p.description{
-        font-weight: 300;
-        line-height: normal;
-        font-size: 14.5px;
-        color: #333333;
-        margin: 30px 0;
+    p.description {
+      font-weight: 300;
+      line-height: normal;
+      font-size: 14.5px;
+      color: #333333;
+      margin: 30px 0;
 
-        a{
-            color: #1d9cdd;
-        }
-
+      a {
+        color: #1d9cdd;
+      }
     }
 
     #break {
-        border: 1px solid rgba(0, 0, 0, 0.2);
-        margin-bottom: 12px;
-    }
-    
-    .contractors-section{
-        @media(min-width: 768px){
-        a{
-            position: absolute;
-            right: 2%;
-            bottom: 7%;
-            font-family: Nunito;
-            font-style: normal;
-            font-weight: 400;
-            line-height: normal;
-            font-size: 13px;
-            color: #333333;
-            background-image : url(${rightChevron});
-            background-position: 90% center;
-            background-size:contain;
-            background-repeat: no-repeat;
-            width: 47%;
-            height: 30px;
-            line-height: 30px;
-        }
+      border: 1px solid rgba(0, 0, 0, 0.2);
+      margin-bottom: 12px;
     }
 
-    @media(max-width: 767px){
-        a{
-        position: relative;
-        bottom: 8px;
-        left: 50%;
-        font-family: Nunito;
-        font-style: normal;
-        font-weight: 400;
-        line-height: normal;
-        font-size: 13px;
-        color: #333333;
-        background-position: 105% center;
-        background-size: contain;
-        background-repeat: no-repeat;
-        width: 47%;
-        height: 30px;
-        line-height: 30px;
-        display: block;
+    .contractors-section {
+      @media (min-width: 768px) {
+        a {
+          position: absolute;
+          right: 2%;
+          bottom: 7%;
+          font-family: Nunito;
+          font-style: normal;
+          font-weight: 400;
+          line-height: normal;
+          font-size: 13px;
+          color: #333333;
+          background-image: url(${rightChevron});
+          background-position: 90% center;
+          background-size: contain;
+          background-repeat: no-repeat;
+          width: 47%;
+          height: 30px;
+          line-height: 30px;
         }
-    }
+      }
 
-    h4{
+      @media (max-width: 767px) {
+        a {
+          position: relative;
+          bottom: 8px;
+          left: 50%;
+          font-family: Nunito;
+          font-style: normal;
+          font-weight: 400;
+          line-height: normal;
+          font-size: 13px;
+          color: #333333;
+          background-position: 105% center;
+          background-size: contain;
+          background-repeat: no-repeat;
+          width: 47%;
+          height: 30px;
+          line-height: 30px;
+          display: block;
+        }
+      }
+
+      h4 {
         margin: 0 5px;
         font-family: Nunito;
         font-style: normal;
@@ -85,129 +84,124 @@ export const Wrapper = styled.div `
         line-height: normal;
         font-size: 16px;
         color: #333333;
-        }
+      }
 
-        img{
-            height: 40px;
-            width: 40px;
-            margin-right: 10px;
-        }
+      img {
+        height: 40px;
+        width: 40px;
+        margin-right: 10px;
+      }
     }
-    .location span, .budget span{
-        display: inline-block;
-        font-style: normal;
-        font-weight: 300;
-        line-height: normal;
-        font-size: 13.5px;
-        color: #333333;
+    .location span,
+    .budget span {
+      display: inline-block;
+      font-style: normal;
+      font-weight: 300;
+      line-height: normal;
+      font-size: 13.5px;
+      color: #333333;
     }
+  }
 
-}
+  height: 100%;
+  margin: 2% 0;
 
-height: 100%;
-margin: 2% 0;
-
-.inner{
+  .inner {
     overflow: auto;
     position: relative;
-    background: #F2F2F2;
+    background: #f2f2f2;
     box-shadow: 0px 4px 25px rgba(0, 0, 0, 0.15);
     border-radius: 5px;
     width: 100%;
     min-height: 375px;
 
-    .projectPicture{
-        height: 375px;
-        width: 100%;
-        object-fit: cover;
-        display: block;
+    .projectPicture {
+      height: 375px;
+      width: 100%;
+      object-fit: cover;
+      display: block;
     }
 
-    >div{
-        height: 100%;
-        overflow: hidden;
-        position: relative;
+    > div {
+      height: 100%;
+      overflow: hidden;
+      position: relative;
     }
-    .top > img{
-        height: 20px;
-        width: 20px;
-        object-fit: contain;
-        position: relative;
-        top: 6px;
-        margin-right: 1.5px;
-    }
-
-    .projectStatus{
-        position: absolute;
-        top:0;
-        left:0;
-        background: ${props => colors[props.statusColor]};
-        box-shadow: 0px 4px 25px rgba(0, 0, 0, 0.15);
-        font-size: 15px;
-        font-weight: 300;
-        padding: 7px 15px;
-        color: white;
-        border-radius: 5px 0px;
+    .top > img {
+      height: 20px;
+      width: 20px;
+      object-fit: contain;
+      position: relative;
+      top: 6px;
+      margin-right: 1.5px;
     }
 
-    .completionPercentage{
-        bottom: 4%;
-        left: 0;
-        right: 0;
-        overflow: hidden;
-        margin: auto;
-        text-align: center;
-        background: white;
-        width: 90%;
-        position: absolute;
-        background: #F2F2F2;
-        box-shadow: 0px 4px 25px rgba(0, 0, 0, 0.15);
-        border-radius: 5px;
-        font-weight: 300;
-        font-size: 14px;
+    .projectStatus {
+      position: absolute;
+      top: 0;
+      left: 0;
+      background: ${props => colors[props.statusColor]};
+      box-shadow: 0px 4px 25px rgba(0, 0, 0, 0.15);
+      font-size: 15px;
+      font-weight: 300;
+      padding: 7px 15px;
+      color: white;
+      border-radius: 5px 0px;
     }
-}
 
+    .completionPercentage {
+      bottom: 4%;
+      left: 0;
+      right: 0;
+      overflow: hidden;
+      margin: auto;
+      text-align: center;
+      background: white;
+      width: 90%;
+      position: absolute;
+      background: #f2f2f2;
+      box-shadow: 0px 4px 25px rgba(0, 0, 0, 0.15);
+      border-radius: 5px;
+      font-weight: 300;
+      font-size: 14px;
+    }
+  }
 
-@media(min-width: 1042px){
-.inner{
-    width: 95%;
-}
-    &:nth-child(odd){
-        .inner{
+  @media (min-width: 1042px) {
+    .inner {
+      width: 95%;
+    }
+    &:nth-child(odd) {
+      .inner {
         float: left;
-        }
+      }
     }
 
-    &:nth-child(even){
-        .inner{
+    &:nth-child(even) {
+      .inner {
         float: right;
-        }
+      }
     }
-}
-`
-
+  }
+`;
 
 export const Percentage = styled.span`
-padding: 8px 0;
-&#blue {
-    background-color: #2D9CDB;
+  padding: 8px 0;
+  &#blue {
+    background-color: #2d9cdb;
     color: white;
     width: ${props => props.width}%;
     display: inline-block;
     float: left;
     height: 35px;
-}
+  }
 
-&#normal {
+  &#normal {
     background-color: white;
-    color: #333333; 
+    color: #333333;
     width: calc(100% - ${props => props.width}%);
     display: inline-block;
     float: right;
     height: 35px;
-
-
-}
-
-`
+  }
+`;
